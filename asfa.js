dd14672b404956ed824822847515d302b84a0a07
ASF-idn <muhammadasfaanas3a@gmail.com>
2026-07-24 08:25:45 +0700

    Delete asfa.js

--- asfa.js
+++
@@ -1,4 +0,0 @@
-console.log("nama : Muhammad Asfa Anas");
-console.log("tanggal lahir : 13 januari 2011");
-console.log("SMP: SMP ISLAM SABILILLAH MALANG");
-console.log("KARENA SAYA INGIN MENJADI PROGRAMMER, SAYA INGIN BELAJAR JAVASCRIPT DAN PYTHON");

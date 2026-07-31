cd888c4ac49b5f98b6059917aa0990ae0a01f3c3
ASF-idn <muhammadasfaanas3a@gmail.com>
2026-07-31 07:59:55 +0700

    Add files via upload

---
+++ java/karyawan.js
@@ -0,0 +1,15 @@
+// deklarasi karyawan
+const karyawan = {
+    nama: "asfa", //string
+    umur: 21, //number
+    gaji: 10000000, //number
+    statuskartap: true, //boolean
+    jabatan: null //null
+}
+// output template literal
+console.log(`nama karyawan      : ${karyawan.nama}`);
+console.log(`umur karyawan      : ${karyawan.umur}`);
+console.log(`gaji karyawan      : ${karyawan.gaji}`);
+console.log(`status karyawan    : ${karyawan.statuskartap}`);
+console.log(`jabatan karyawan   : ${karyawan.jabatan}`);
+
